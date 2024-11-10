abaa82629a541bbd546f3f9905c80be5c7ece34a
thewoowon <thewoowon@naver.com>
2024-11-10 14:17:48 +0900

    feat: 환경변수 작업

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    'react-native-reanimated/plugin', // Reanimated 플러그인 추가
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: '.env',
+        blacklist: null,
+        whitelist: null,
+        safe: false,
+        allowUndefined: true,
+      },
+    ],
+  ],
 };
+
+// 캐시 삭제 필요.
+// npx react-native start --reset-cache
