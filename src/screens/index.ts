7eebc005019e1e875921c4d68d1ca48269f280d4
thewoowon <thewoowon@naver.com>
2024-11-17 23:42:30 +0900

    feat: index export edited

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -2,3 +2,4 @@ export {default as HomenScreen} from './HomeScreen';
 export {default as DeliveryScreen} from './DeliveryScreen';
 export {default as OrderScreen} from './OrderScreen';
 export {default as MyScreen} from './MyScreen';
+export {default as NotificationScreen} from './NotificationScreen';
