36e00916698914aa7c392098dd4d9b9f1f4fc8b6
thewoowon <thewoowon@naver.com>
2024-11-17 23:43:13 +0900

    feat: order main organized

--- src/screens/OrderMainScreen.tsx
+++ src/screens/OrderMainScreen.tsx
@@ -9,8 +9,11 @@ import {
   TextInput,
   Pressable,
   Animated,
+  ScrollView,
 } from 'react-native';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {RingIcon, SearchIcon} from '../components/Icons';
+import {ORDER_DATA} from '../data';
 
 type RootStackParamList = {
   OrderMain: undefined;
@@ -20,7 +23,8 @@ type RootStackParamList = {
   Camera: undefined;
 };
 
-const OrderMainScreen = ({navigation}: any) => {
+const OrderMainScreen = ({navigation, route}: any) => {
+  // const {query, userId} = route.params;
   return (
     <GestureHandlerRootView style={styles.container}>
       <StatusBar
@@ -29,16 +33,55 @@ const OrderMainScreen = ({navigation}: any) => {
         translucent={false}
       />
       <SafeAreaView style={styles.backgroundStyle}>
-        <View style={styles.container}>
-          <Text>Order Screen</Text>
+        <View style={styles.header}>
+          {/* 상단 헤더 */}
+          <Text style={styles.headerText}>주문</Text>
+          <View
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              gap: 16,
+            }}>
+            <Pressable
+              onPress={() => {
+                navigation.navigate('Search');
+              }}>
+              <SearchIcon />
+            </Pressable>
+            <Pressable
+              onPress={() => {
+                // TODO:
+                // 전역 알림창 띄우기
+                navigation.navigate('Notification');
+              }}>
+              <RingIcon />
+            </Pressable>
+          </View>
         </View>
-        <Pressable
-          onPress={() => {
-            // stack navigation
-            navigation.navigate('Search');
-          }}>
-          <Text>Pressable</Text>
-        </Pressable>
+        <ScrollView style={styles.orderList}>
+          {ORDER_DATA.length > 0 ? (
+            ORDER_DATA.map(order => {
+              return (
+                <Pressable
+                  key={order.id}
+                  onPress={() => {
+                    navigation.navigate('CompanyInfo');
+                  }}>
+                  <Text>{order.title}</Text>
+                  <Text>{order.description}</Text>
+                  <Text>{order.dueDate}</Text>
+                </Pressable>
+              );
+            })
+          ) : (
+            <View
+              style={{
+                flex: 1,
+              }}>
+              <Text>진행중인 주문이 없습니다</Text>
+            </View>
+          )}
+        </ScrollView>
       </SafeAreaView>
     </GestureHandlerRootView>
   );
@@ -48,8 +91,29 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  header: {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingRight: 30,
+    paddingLeft: 20,
+    paddingTop: 11,
+    paddingBottom: 11,
+    maxHeight: 50,
+  },
   backgroundStyle: {
     flex: 1,
+  },
+  headerText: {
+    color: '#C7CDD1',
+    fontSize: 20,
+    fontFamily: 'Pretendard-ExtraBold',
+  },
+  orderList: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 20,
   },
 });
 
