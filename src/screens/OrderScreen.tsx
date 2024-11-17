58b2181bb50347b61b130ba8d661cd59f88057f2
thewoowon <thewoowon@naver.com>
2024-11-17 23:42:51 +0900

    feat: order screen header option

--- src/screens/OrderScreen.tsx
+++ src/screens/OrderScreen.tsx
@@ -5,6 +5,7 @@ import OrderMainScreen from './OrderMainScreen';
 import CompanyInfoScreen from './CompanyInfoScreen';
 import OrderSheetScreen from './OrderSheetScreen';
 import CameraScreen from './CameraScreen';
+import NotificationScreen from './NotificationScreen';
 
 // 스택 필요
 
@@ -13,11 +14,36 @@ const OrderStack = createStackNavigator();
 const OrderScreen = () => {
   return (
     <OrderStack.Navigator>
-      <OrderStack.Screen name="OrderMain" component={OrderMainScreen} />
-      <OrderStack.Screen name="Search" component={SearchScreen} />
-      <OrderStack.Screen name="CompanyInfo" component={CompanyInfoScreen} />
-      <OrderStack.Screen name="OrderSheet" component={OrderSheetScreen} />
-      <OrderStack.Screen name="Camera" component={CameraScreen} />
+      <OrderStack.Screen
+        name="OrderMain"
+        component={OrderMainScreen}
+        options={{headerShown: false}}
+      />
+      <OrderStack.Screen
+        name="Search"
+        component={SearchScreen}
+        options={{headerShown: false}}
+      />
+      <OrderStack.Screen
+        name="CompanyInfo"
+        component={CompanyInfoScreen}
+        options={{headerShown: false}}
+      />
+      <OrderStack.Screen
+        name="OrderSheet"
+        component={OrderSheetScreen}
+        options={{headerShown: false}}
+      />
+      <OrderStack.Screen
+        name="Camera"
+        component={CameraScreen}
+        options={{headerShown: false}}
+      />
+      <OrderStack.Screen
+        name="Notification"
+        component={NotificationScreen}
+        options={{headerShown: false}}
+      />
     </OrderStack.Navigator>
   );
 };
