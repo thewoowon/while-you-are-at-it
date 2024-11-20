120debe4000fef38c9bc65e75b1329e4e9e51deb
thewoowon <thewoowon@naver.com>
2024-11-20 12:41:31 +0900

    feat: company info screen edited

--- src/screens/CompanyInfoScreen.tsx
+++ src/screens/CompanyInfoScreen.tsx
@@ -4,15 +4,20 @@ import {
   StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
-  TextInput,
   Pressable,
-  Animated,
 } from 'react-native';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {
+  LeftChevronIcon,
+  MiniPhoneIcon,
+  MiniStarIcon,
+  MiniWatchIcon,
+} from '../components/Icons';
+import NaverMap from '../components/NaverMap';
 
-const CompanyInfoScreen = () => {
+const CompanyInfoScreen = ({navigation, route}: any) => {
+  const {company} = route.params;
   return (
     <GestureHandlerRootView style={styles.container}>
       <StatusBar
@@ -21,8 +26,100 @@ const CompanyInfoScreen = () => {
         translucent={false}
       />
       <SafeAreaView style={styles.backgroundStyle}>
+        <View style={styles.header}>
+          {/* 상단 헤더 */}
+          <View
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              position: 'absolute',
+              left: 16,
+            }}>
+            <Pressable
+              onPress={() => {
+                navigation.goBack();
+              }}>
+              <LeftChevronIcon />
+            </Pressable>
+          </View>
+          <Text style={styles.headerText}>{company.name}</Text>
+        </View>
         <View style={styles.container}>
-          <Text>Order Screen</Text>
+          <View
+            style={{
+              height: 141,
+              maxHeight: 141,
+            }}>
+            <NaverMap />
+          </View>
+          <View>
+            <Text>{company.name}</Text>
+            <View
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'center',
+                gap: 4,
+              }}>
+              <MiniStarIcon />
+              <Text
+                style={{
+                  color: '#394245',
+                  fontSize: 14,
+                  fontFamily: 'Pretendard-Medium',
+                }}>{`5.0 (후기 250개)`}</Text>
+            </View>
+            <View
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'center',
+                gap: 4,
+              }}>
+              <MiniWatchIcon />
+              <Text
+                style={{
+                  color: '#394245',
+                  fontSize: 14,
+                  fontFamily: 'Pretendard-Medium',
+                }}>
+                영업시간
+              </Text>
+              <Text
+                style={{
+                  color: '#394245',
+                  fontSize: 14,
+                  fontFamily: 'Pretendard-Medium',
+                }}>
+                오전 09:30 ~ 오후 06:00
+              </Text>
+            </View>
+            <View
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'center',
+                gap: 4,
+              }}>
+              <MiniPhoneIcon />
+              <Text
+                style={{
+                  color: '#394245',
+                  fontSize: 14,
+                  fontFamily: 'Pretendard-Medium',
+                }}>
+                전화번호
+              </Text>
+              <Text
+                style={{
+                  color: '#394245',
+                  fontSize: 14,
+                  fontFamily: 'Pretendard-Medium',
+                }}>
+                02.123.4567
+              </Text>
+            </View>
+          </View>
         </View>
       </SafeAreaView>
     </GestureHandlerRootView>
@@ -35,6 +132,22 @@ const styles = StyleSheet.create({
   },
   backgroundStyle: {
     flex: 1,
+  },
+
+  header: {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 11,
+    paddingBottom: 11,
+    maxHeight: 50,
+  },
+  headerText: {
+    color: '#181818',
+    fontSize: 18,
+    fontFamily: 'Pretendard-SeimBold',
   },
 });
 
