447a53dcf27e849bfb0002de5ee229345dd19c87
thewoowon <thewoowon@naver.com>
2024-11-20 11:40:15 +0900

    feat: material icon components

---
+++ src/components/Icons/material/index.ts
@@ -0,0 +1,9 @@
+export {default as AcrylIcon} from './AcrylIcon';
+export {default as EngravingIcon} from './EngravingIcon';
+export {default as HammerIcon} from './HammerIcon';
+export {default as OthersIcon} from './OthersIcon';
+export {default as PaintIcon} from './PaintIcon';
+export {default as PlasticIcon} from './PlasticIcon';
+export {default as PrintIcon} from './PrintIcon';
+export {default as PrintingIcon} from './PrintingIcon';
+export {default as TreeIcon} from './TreeIcon';
