13806ac007f82d58b026356e2acef1b37f7657e4
thewoowon <thewoowon@naver.com>
2024-11-23 13:42:46 +0900

    feat: company type added

--- index.d.ts
+++ index.d.ts
@@ -47,12 +47,69 @@ type NotificationType = {
 
 type SearchType = {
   id: string;
+  // company id
+  companyId: string;
   // 상호
   name: string;
-  type: string;
+  type: string[];
   // 1 회당 가격
   price: number;
   // 주소
+  address: string;
+  src: string;
+};
+
+type CompanyType = {
+  id: string;
+  // 상호
+  name: string;
+  // 카테고리
+  type: string[];
+  // 영업시간 오픈-클로즈
+  businessHours: string;
+  // 전화번호
+  phoneNumber: string;
+  // 주소
+  address: string;
+  // 서비스
+  services: ServiceType[];
+};
+// 제공 서비스
+type ServiceType = {
+  id: string;
+  // 서비스명
+  name: string;
+  // 서비스 설명
+  description: string;
+  // unit
+  unit: string;
+  // 가격
+  price: number;
+  // 할인율
+  discountRate: number;
+  // 할인가
+  discountPrice: number;
+  // 이미지
+  src: string;
+  // type
+  type: string;
+};
+
+type ReviewType = {
+  id: string;
+  companyId: string;
+  title: string;
+  score: number;
+  review: string;
+  date: string;
+  user: UserType;
+};
+
+type UserType = {
+  id: string;
+  name: string;
+  email: string;
+  phoneNumber: string;
   address: string;
   src: string;
 };
