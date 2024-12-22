a9db4319ed9bca2b0a76161a8b58e7d971bba8b2
thewoowon <thewoowon@naver.com>
2024-12-22 21:22:35 +0900

    feat: new ios setting

--- ios/WhileYouAreAtIt/AppDelegate.mm
+++ ios/WhileYouAreAtIt/AppDelegate.mm
@@ -1,6 +1,7 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
+#import <RNGoogleSignin/RNGoogleSignin.h> // Google Sign-In 모듈 추가
 
 @implementation AppDelegate
 
@@ -11,9 +12,21 @@
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
 
+  // Google Sign-In 초기화
+  [RNGoogleSignin application:application didFinishLaunchingWithOptions:launchOptions];
+
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
+// Google Sign-In URL 처리 메서드 추가
+- (BOOL)application:(UIApplication *)app
+            openURL:(NSURL *)url
+            options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
+{
+  return [RNGoogleSignin application:app openURL:url options:options];
+}
+
+// React Native의 JS 번들 URL 설정
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
   return [self bundleURL];
