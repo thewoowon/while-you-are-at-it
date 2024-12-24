985327ecb9147f4526b430e803545a189ab4584a
thewoowon <thewoowon@naver.com>
2024-12-24 23:10:53 +0900

    feat: ios build file

--- ios/WhileYouAreAtIt/AppDelegate.mm
+++ ios/WhileYouAreAtIt/AppDelegate.mm
@@ -1,32 +1,27 @@
 #import "AppDelegate.h"
-
 #import <React/RCTBundleURLProvider.h>
-#import <RNGoogleSignin/RNGoogleSignin.h> // Google Sign-In 모듈 추가
+#import <GoogleSignIn/GoogleSignIn.h> // Google Sign-In 헤더 추가
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   self.moduleName = @"WhileYouAreAtIt";
-  // You can add your custom initial props in the dictionary below.
-  // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
-
-  // Google Sign-In 초기화
-  [RNGoogleSignin application:application didFinishLaunchingWithOptions:launchOptions];
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
-// Google Sign-In URL 처리 메서드 추가
+// URL 처리 메서드
 - (BOOL)application:(UIApplication *)app
             openURL:(NSURL *)url
             options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
 {
-  return [RNGoogleSignin application:app openURL:url options:options];
+  NSLog(@"Google Sign-In URL: %@", url.absoluteString);
+  return [[GIDSignIn sharedInstance] handleURL:url];
 }
 
-// React Native의 JS 번들 URL 설정
+// JS 번들 URL 설정
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
   return [self bundleURL];
